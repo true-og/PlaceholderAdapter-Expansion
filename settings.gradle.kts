7f25d95fc42ff7f0091bcfdaff3ee8e29cd14be4
NotAlexNoyle <alexnoyle@icloud.com>
2025-09-27 12:49:48 -0400

    Fix building with TrueOG Bootstrap

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-rootProject.name = "PlaceholderAPI-Expansion"
+rootProject.name = "PlaceholderAdapter-Expansion"
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
